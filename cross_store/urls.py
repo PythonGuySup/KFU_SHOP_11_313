dcc50b428d2d2f76727718f0ccb70aec80a9c650
PythonGuySup <egorkachanov2006@gmail.com>
2023-12-07 12:38:22 +0300

    Добавлены модели БД

--- cross_store/urls.py
+++ cross_store/urls.py
@@ -5,8 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name = "index" ),
-    path('index', views.index, name = "index"),
+    path('', views.index, name="index"),
+    path('index', views.index, name="index"),
+    path('index.html', views.index, name="index"),
+    path('shop.html', views.shop, name="shop"),
+    path('blog.html', views.blog, name="blog"),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
